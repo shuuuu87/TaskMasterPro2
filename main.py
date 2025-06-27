7f445aedefb5799302d3ab61a377560acd0f101e
Shreyash12345678 <shreyashmodkule121@gmail.com>
2025-06-27 09:06:32 +0530

    Updated files

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, Response
+from app import app  # this imports the real app from app.py
+from flask import send_from_directory
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
+# Serve robots.txt from the static folder
 @app.route('/robots.txt')
-def robots_txt():
-    return Response("""
-    User-agent: *
-    Disallow: /login
-    Disallow: /register
-    Disallow: /profile
-    Disallow: /dashboard
-    Disallow: /admin
-    Allow: /
-
-    Sitemap: https://taskmasterpro-b1nt.onrender.com/sitemap.xml
-    """, mimetype='text/plain')
+def static_robots():
+    return send_from_directory('static', 'robots.txt')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
